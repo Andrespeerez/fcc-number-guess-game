40e9fbc7a2f5d21cf7bb9fe63e178c5f8547c396
Andrespeerez <andreslavila93@gmail.com>
2023-01-09 16:42:09 +0000

    fix: username max lenght incresed

--- number_guess.sql
+++ number_guess.sql
@@ -49,7 +49,7 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.users (
     user_id integer NOT NULL,
-    username character varying(22) NOT NULL,
+    username character varying(100) NOT NULL,
     best_game integer,
     games_played integer
 );
@@ -90,16 +90,29 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.users VALUES (1, 'test', 1, 10000);
 INSERT INTO public.users VALUES (2, 'user_1673279813540', NULL, NULL);
 INSERT INTO public.users VALUES (3, 'user_1673279813539', NULL, NULL);
+INSERT INTO public.users VALUES (1, 'test', 13, 10002);
+INSERT INTO public.users VALUES (5, 'user_1673280419364', NULL, 2);
+INSERT INTO public.users VALUES (4, 'user_1673280419365', NULL, 5);
+INSERT INTO public.users VALUES (6, 'Andres', NULL, NULL);
+INSERT INTO public.users VALUES (8, 'user_1673281147124', NULL, 2);
+INSERT INTO public.users VALUES (7, 'user_1673281147125', NULL, 5);
+INSERT INTO public.users VALUES (10, 'user_1673281241132', NULL, 2);
+INSERT INTO public.users VALUES (9, 'user_1673281241133', NULL, 5);
+INSERT INTO public.users VALUES (12, 'user_1673281329330', NULL, 2);
+INSERT INTO public.users VALUES (11, 'user_1673281329331', NULL, 5);
+INSERT INTO public.users VALUES (14, 'user_1673281851662', NULL, 2);
+INSERT INTO public.users VALUES (13, 'user_1673281851663', NULL, 5);
+INSERT INTO public.users VALUES (16, 'user_1673282390465', NULL, 2);
+INSERT INTO public.users VALUES (15, 'user_1673282390466', NULL, 5);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 3, true);
+SELECT pg_catalog.setval('public.users_user_id_seq', 16, true);
 
 
 --
